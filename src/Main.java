bf989654757eb679fe8ed12ffd5c9c45c2ec8cc7
Сергей <legenda.serzh@mail.ru>
2024-04-21 23:52:49 +0300

    hw1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,66 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.print("task1\n");
+        int[] arr = new int[]{1, 2, 3};
+        double[] arr1 = {1.57, 7.654, 9.986};
+        int[] arr2 = new int[12];
+
+        System.out.print("task2\n");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i != arr.length - 1) {
+                System.out.print(" , ");
+            }
+        }
+        System.out.println();
+
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.print(arr1[i]);
+            if (i != arr1.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i]);
+            if (i != arr2.length - 1) {
+                System.out.print(" , ");
+            }
+        }
+        System.out.println();
+
+        System.out.print("task3\n");
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        for (int i = arr1.length - 1; i >= 0; i--) {
+            System.out.print(arr1[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = arr2.length - 1; i >= 0; i--) {
+            System.out.print(arr2[i]);
+            if (i != 0) {
+                System.out.print(", ");
+
+            }
+        }
+        System.out.println();
+
+        System.out.print("task4\n");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                arr[i]++;
+            }
+            System.out.print(arr[i] + "");
+        }
     }
 }
